7baed3a65fad1c3df0293fc763a4b6855ec444ac
ekaro <marek.pekarovic@gmail.com>
2017-04-10 15:41:06 +0200

    Add files via upload

--- yt.py
+++ yt.py
@@ -1,115 +1,111 @@
-import pafy #needs youtube-dl
+import pafy # needs youtube-dl
 import validators
-import re
 
 defdir = "C:/Downloads"
-print("Default download directory is: %s" %defdir)
+print("\nDefault download directory is: %s" %defdir)
 
+# setting download directory
 valid_input = False
 while not valid_input:
-    defchange = input("Do you want to download to this directory? (Y/N) ")
-
+    defchange = input("\nDo you want to download to this directory? (y/n) ")  
     if defchange.upper() == "N":
-        ddir = input("Enter new directory: ")
-        #ddir.replace("\\", "/")
-        print("Video will be downloaded to %s: " %ddir)
+        ddir = input("\nEnter new directory: ")
+        ddir.replace("\\", "/")
+        print("\nVideo will be downloaded to %s: " %ddir)
         valid_input = True
-        break
-        '''
-        valid_input2 = False
-        while not valid_input2:
-            set = input("Do you want to set this directory as default? (Y/N) ")
-            if set.upper() == "Y":
-                defdir = ddir
-                print("New default download directory is: %s" %defdir)
-                valid_input2 = True
-            if set.upper() == "N":
-                valid_input2 = True
-            else:
-                print("Wrong input, please enter Y or N")'''
-               
+        break           
     if defchange.upper() == "Y":
         ddir = defdir
         valid_input = True
-        break
-
+        break       
     else:
-        print("Wrong input, please enter Y or N")
+        print("\nWrong input, please enter Y or N")
 
+# validation of url
 valid_url = False
 while not valid_url:
-
-    url = input("Enter url of video: ")
-    valid_url = validators.url(url)
-
+    url = input("\nEnter url of youtube video: ")
+    valid_url = validators.url(url) 
     if valid_url:
-        if re.search("https://www.youtube.com", url):
+        try:
             video = pafy.new(url)
-            print("Your video is: ", video.title, url)
-        else:
+            print(video.title)
+        except: 
             valid_url = False
-            print("Not valid youtube url, please enter again.")
+            print("\n%s is not a valid youtube url, please enter again." %url)                            
     else:
-        print("Not valid url, please enter again.")
-        
+        print("\n%s is not a valid url, please enter again." %url)
+
+# printing possible formats of video        
 streams = video.streams
-print("You can download these sizes of the video:")
-
+print("\nYou can download these versions of the video:")
 for stream in streams:        
-    print(streams.index(stream)+1, "Resolution:", stream.resolution, "File:", stream.extension, "Size:", round(stream.get_filesize()/1024/1024, 2), "MB")
+    print("%s." %(streams.index(stream)+1), "Resolution: %s," %(stream.resolution), "Format: %s," %(stream.extension), "Size: %s MB" %(round(stream.get_filesize()/1024/1024, 2)))
 
+# selecting quality of video and downloading
 valid_input = False
-while not valid_input:
-
-    down = input("Select which video you want to download (1...%s) or select B for best quality: " %(len(streams)))
-
+while not valid_input:   
+    down = input("\nSelect which quality of the video you want to download (1...%s) or select B for best quality: " %(len(streams)))
+    
     if down.upper() == 'B':
         best = video.getbest()
-        print("The best quality is: Resolution -", best.resolution, "Format -", best.extension, "Size -", round(best.get_filesize()/1024/1024, 2), "MB")      
-
+        print("\nResolution: %s," %(best.resolution), "Format: %s," %(best.extension), "Size: %s MB" %(round(best.get_filesize()/1024/1024, 2)))      
         valid_input = False
         while not valid_input:
-            yn = input("Do you wish to download? (Y/N) ")
+            yn = input("\nDo you wish to download this video? (y/n) ")
             if yn.upper() == 'Y':
                 try:
-                    print("Downloading...")
-                    best.download(quiet=True, filepath=ddir)                    
-                    print("Download successful!")
+                    print("\nDownloading...")
+                    best.download(quiet=True, filepath=ddir) # TODO: if file exists...                  
+                    print("\nDownload successful!")
                     valid_input = True
                     break
                 except:
-                    print("Download not successful. Please try again.")
+                    print("\nDownload not successful. Please try again later.")
+                    valid_input = True
+                    break
             if yn.upper() == 'N':
-                print("Download canceled")
-                valid_input = True
+                valid_input = True                
             else:
-                print("Wrong input, please enter Y or N")
+                print("\nWrong input, please enter Y or N.")
 
-        valid_input = True
+        if yn.upper() == 'Y':
+            valid_input = True            
+        if yn.upper() == 'N':
+            valid_input = False                
 
-    idown = int(down)       
-    if idown <= len(streams) and idown > 0:
-        print("You chose this video: Resolution -", streams[idown-1].resolution, "Format -", streams[idown-1].extension, "Size -", round(streams[idown-1].get_filesize()/1024/1024, 2), "MB") 
-
+    try:    
+        idown = int(down)
+        downisnum = True
+    except:
+        downisnum = False        
+        break
+    
+    if downisnum and idown <= len(streams) and idown > 0:
+        print("\nResolution: %s," %(streams[idown-1].resolution), "Format: %s," %(streams[idown-1].extension), "Size: %s MB" %(round(streams[idown-1].get_filesize()/1024/1024, 2))) 
         valid_input = False
         while not valid_input:
-            yn = input("Do you wish to download this video? (Y/N) ")
+            yn = input("\nDo you wish to download this video? (y/n) ")
             if yn.upper() == 'Y':
                 try:
-                    print("Downloading...")
-                    streams[idown-1].download(quiet=True, filepath=ddir)
-                    print("Download successful!")
+                    print("\nDownloading...")
+                    streams[idown-1].download(quiet=True, filepath=ddir) # TODO: if file exists...
+                    print("\nDownload successful!")
                     valid_input = True
                     break
                 except:
-                    print("Download not successful. Please try again.")
-            if yn.upper() == 'N':
-                print("Download canceled")
-                valid_input = True
+                    print("\nDownload not successful. Please try again later.")
+                    valid_input = True
+                    break
+            if yn.upper() == 'N':                
+                valid_input = True                
             else:
-                print("Wrong input, please enter Y or N")
+                print("\nWrong input, please enter Y or N.")
 
-        valid_input = True
-
+        if yn.upper() == 'Y':
+            valid_input = True
+        if yn.upper() == 'N':
+            valid_input = False
+        
     else:
-        print("Wrong input, please enter number between 1 and %s or enter B for best quality" %(len(streams)))
+        print("\nWrong input, please enter number between 1 and %s or enter B for best quality." %(len(streams))) # need to fix showing of this after download is successful
